7ac96f041a420cb46a6f8c76d0b5dd6493a1f75d
dock <hyun4you2006@naver.com>
2021-10-11 18:31:08 +0900

    코드 간소화, ws, localtunnel 설치

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -7,29 +7,12 @@ const ul = document.querySelector('ul');
 
 let i = 0;
 
-function handleSubmit(e) {
-    e.preventDefault();
-
-    switch (i) {
-        case 0:
-            i++;
-            socket.emit('nickname', input.value);
-            input.value = '';
-            input.placeholder = 'Room Name';
-            btn.innerText = 'Enter Room';
-            break;
-        case 1:
-            i++;
-            socket.emit('room', input.value);
-            input.value = '';
-            input.placeholder = 'Message';
-            btn.innerText = 'Send';
-            break;
-        default:
-            socket.emit('msg', input.value);
-            input.value = '';
-            break;
-    }
+function handleSubmit(evtName, placeholder, btnText) {
+    if (i <= 1) i++; /* 입력 수 증가 (닉네임, 방, 메시지 입력 구별 용도) */
+    socket.emit(evtName, input.value); /* 이벤트 발생 */
+    input.value = ''; /* 입력 비움 */
+    input.placeholder = placeholder; /* placeholder 변경 */
+    btn.innerText = btnText; /* button innerText 변경 */
 }
 
 /* 챗 추가 */
@@ -39,6 +22,21 @@ function addMsg(msg) {
     ul.appendChild(li); /* chat에 li 추가 */
 }
 
-form.addEventListener('submit', handleSubmit); /* 닉네임 입력 감지 */
+/* 입력 감지 */
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    switch (i) {
+        case 0 /* 닉네임 입력 후 */:
+            handleSubmit('nickname', 'Room Name', 'Enter Room');
+            break;
+        case 1 /* 방 이름 입력 후 */:
+            handleSubmit('room', 'Message', 'Send');
+            break;
+        default:
+            /* 메시지 입력 */
+            handleSubmit('msg', 'Message', 'Send');
+            break;
+    }
+});
 
 socket.on('msg', addMsg); /* msg 이벤트 감지, addMsg 실행 */
